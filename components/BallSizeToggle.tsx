949486a486c4e5ff931edbc8a1a1f52a8380903a
Lario Borges <lario.borges@gmail.com>
2023-11-09 17:11:39 +0200

    Eslint and prettier configuration

--- components/BallSizeToggle.tsx
+++ components/BallSizeToggle.tsx
@@ -1,12 +1,12 @@
 import { Button } from 'react-native';
 
 export default function BallSizeToggle(props) {
-    return (
-        <Button
-            title={`Change Ball Size from ${props.ballSize}`}
-            onPress={() => {
-                props.toggleBallSize();
-            }}
-        />
-    );
+  return (
+    <Button
+      title={`Change Ball Size from ${props.ballSize}`}
+      onPress={() => {
+        props.toggleBallSize();
+      }}
+    />
+  );
 }
--- hooks/usBallSizeState.ts
+++ hooks/usBallSizeState.ts
@@ -1,33 +1,34 @@
-import { useAsyncStorage } from "@react-native-async-storage/async-storage";
-import { useEffect, useState } from "react";
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 
 export const BALL_SIZE = {
-    SMALL: 'SMALL',
-    LARGE: 'LARGE',
+  SMALL: 'SMALL',
+  LARGE: 'LARGE',
 };
 
 export default function useBallSizeState() {
-    const [ballSize, setBallSize] = useState('ball_size');
-    const { getItem: getBallSizeItem, setItem: setBallSizeItem } = useAsyncStorage('ball_size');
+  const [ballSize, setBallSize] = useState('ball_size');
+  const { getItem: getBallSizeItem, setItem: setBallSizeItem } = useAsyncStorage('ball_size');
 
-    const readBallSizeFromStorage = async () => {
-        const storedBallSize = await getBallSizeItem();
-        const ballSize = storedBallSize !== BALL_SIZE.SMALL || storedBallSize !== BALL_SIZE.LARGE ? BALL_SIZE.SMALL : storedBallSize;
-        setBallSize(ballSize);
-    };
+  const readBallSizeFromStorage = async () => {
+    const storedBallSize = await getBallSizeItem();
+    const ballSize =
+      storedBallSize !== BALL_SIZE.SMALL || storedBallSize !== BALL_SIZE.LARGE ? BALL_SIZE.SMALL : storedBallSize;
+    setBallSize(ballSize);
+  };
 
-    const toggleBallSize = async () => {
-        const newValue = ballSize === BALL_SIZE.SMALL ? BALL_SIZE.LARGE : BALL_SIZE.SMALL
-        await setBallSizeItem(newValue);
-        setBallSize(newValue);
-    };
+  const toggleBallSize = async () => {
+    const newValue = ballSize === BALL_SIZE.SMALL ? BALL_SIZE.LARGE : BALL_SIZE.SMALL;
+    await setBallSizeItem(newValue);
+    setBallSize(newValue);
+  };
 
-    useEffect(() => {
-        readBallSizeFromStorage();
-    }, []);
+  useEffect(() => {
+    readBallSizeFromStorage();
+  }, []);
 
-    return {
-        ballSize,
-        toggleBallSize,
-    };
+  return {
+    ballSize,
+    toggleBallSize,
+  };
 }
